ecbb6ee0e81adefe9d75139e71a20f13e36bbcf7
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-09-27 16:23:26 +0800

    Update purchaseDLtwo.js
    adding more datalayers

--- assets/js/purchaseDLtwo.js
+++ assets/js/purchaseDLtwo.js
@@ -12,6 +12,145 @@ dataLayer.push(
             "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
             "ignore_referrer": "true"
         }
+    },
+    {
+        "0": "event",
+        "1": "purchase",
+        "2": {
+            "currency": "JPY",
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_path": "/checkout/thank_you",
+            "language": "ja",
+            "page_title": "Checkout - Receipt",
+            "screen_resolution": "3200x1800",
+            "client_id": "1679088078.1677654582",
+            "event_category": "EnhancedEcommerce",
+            "allow_display_features": true,
+            "allow_ad_personalization_signals": true,
+            "cookie_domain": "lucille.co.jp",
+            "cookie_expires": 63072000,
+            "cookie_flags": "",
+            "cookie_path": "/",
+            "cookie_update": true,
+            "is_legacy_converted": true,
+            "send_to": [
+                "G-ZZZ"
+            ],
+            "transaction_id": "5340655878402",
+            "tax": "80",
+            "shipping": "0",
+            "value": "880",
+            "items": [
+                {
+                    "item_id": "WTN-14",
+                    "item_name": "3Dホワイトニングシート 14回分 - 14回分",
+                    "quantity": "1",
+                    "price": "880",
+                    "item_brand": "Lucille オンラインストア",
+                    "item_variant": "14回分"
+                }
+            ]
+        }
+    },
+    {
+        "0": "event",
+        "1": "Completed Order",
+        "2": {
+            "currency": "JPY",
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_path": "/checkout/thank_you",
+            "language": "ja",
+            "page_title": "Checkout - Receipt",
+            "screen_resolution": "3200x1800",
+            "client_id": "1679088078.1677654582",
+            "event_category": "EnhancedEcommerce",
+            "allow_display_features": true,
+            "allow_ad_personalization_signals": true,
+            "cookie_domain": "lucille.co.jp",
+            "cookie_expires": 63072000,
+            "cookie_flags": "",
+            "cookie_path": "/",
+            "cookie_update": true,
+            "is_legacy_converted": true,
+            "send_to": [
+                "G-ZZZ"
+            ]
+        }
+    },
+    {
+        "0": "event",
+        "1": "page_view",
+        "2": {
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_path": "/checkout/thank_you",
+            "language": "ja",
+            "page_title": "Checkout - Receipt",
+            "screen_resolution": "3200x1800",
+            "client_id": "1679088078.1677654582",
+            "allow_display_features": true,
+            "allow_ad_personalization_signals": true,
+            "cookie_domain": "lucille.co.jp",
+            "cookie_expires": 63072000,
+            "cookie_flags": "",
+            "cookie_path": "/",
+            "cookie_update": true,
+            "is_legacy_converted": true,
+            "send_to": [
+                "G-ZZZ"
+            ]
+        }
+    },
+    {
+        "0": "event",
+        "1": "Viewed Checkout - Receipt Page",
+        "2": {
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_path": "/checkout/thank_you",
+            "language": "ja",
+            "page_title": "Checkout - Receipt",
+            "screen_resolution": "3200x1800",
+            "client_id": "1679088078.1677654582",
+            "event_category": "All",
+            "value": "0",
+            "allow_display_features": true,
+            "allow_ad_personalization_signals": true,
+            "cookie_domain": "lucille.co.jp",
+            "cookie_expires": 63072000,
+            "cookie_flags": "",
+            "cookie_path": "/",
+            "cookie_update": true,
+            "is_legacy_converted": true,
+            "send_to": [
+                "G-ZZZ"
+            ]
+        }
+    },   
+    {
+        "0": "event",
+        "1": "purchase",
+        "2": {
+            "send_to": [
+                "G-ZZZ",
+                "MC-ZZZ"
+            ],
+            "transaction_id": 5340655878402,
+            "value": "880",
+            "currency": "JPY",
+            "tax": "80",
+            "shipping": "0",
+            "items": [
+                {
+                    "id": "shopify_JP_7719303971074_43692121063682",
+                    "name": "3Dホワイトニングシート 14回分 - 14回分",
+                    "brand": "Lucille オンラインストア",
+                    "category": "",
+                    "price": "880",
+                    "quantity": 1,
+                    "variant": "14回分"
+                }
+            ],
+            "ignore_referrer": "true"
+        }
     }
 );
 
