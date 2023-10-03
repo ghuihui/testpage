cc4227b0081b0a69b89ac1c5620932223e7ff4da
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-03 16:10:58 +0800

    mnnnn

--- assets/js/forTesting/validateForm2.js
+++ assets/js/forTesting/validateForm2.js
@@ -2,7 +2,7 @@ function validateForm2(form) {
     var test_name = form.elements['name'].value;
     var test_email = form.elements['email'].value;
 
-    if (test_name == "" && test_email == "") {
+    if (test_name == "" || test_email == "") {
       alert("Fill out required fields!");
       return false;
     } else {
