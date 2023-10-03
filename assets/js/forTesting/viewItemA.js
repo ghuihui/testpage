d44f8594bfa95f0e63817052fd0615d5251d962e
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-03 13:10:08 +0800

    asdfasdfa

---
+++ assets/js/forTesting/viewItemA.js
@@ -0,0 +1,27 @@
+dataLayer.push(
+    {
+    'event':'view_item',
+    'currency':'JPY',
+    'value': 880
+    },
+    {
+        "0": "event",
+        "1": "view_item",
+        "2": {
+            "send_to": [
+                "G-ZZZ",
+                "MC-ZZZ"
+            ],
+            "page_title": "item A",
+            "page_location": "https://ghuihui.github.io/testpage/itemA.html",
+            "ignore_referrer": "true"
+        },
+        "3":{
+                "item":[{
+                    "name":"itemA",
+                    "price":880
+                }   
+                ]
+            } 
+    },
+);
