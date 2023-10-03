33f336cb1afd9d9434e5b76a3b3c60edd5594bf1
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-03 15:54:09 +0800

    iiiiii

--- assets/js/forTesting/validateForm.js
+++ assets/js/forTesting/validateForm.js
@@ -1,9 +1,11 @@
 function validateForm(form) {
     var test_name = form.elements['demo-name'].value;
     var test_email = form.elements['demo-email'].value;
-    var test_category = form.elements['demo-category'].value;
+    var test_dropDown = form.elements['demo-category'].value;
+    var test_radioButton = form.elements['demo-priority'].checked; 
+    var test_checkBox = form.elements['demo-human'].checked
 
-    if (test_name == "") {
+    if (test_name == "" && test_email == "" && test_dropDown == "" & !(test_radioButton) && !(test_checkBox)) {
       alert("Name must be filled out");
       return false;
     } else {
