1a53d2ec700fca74fa5bf95b5ab5f6c1fc7bc086
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-04 10:27:38 +0800

    ooooooo

--- assets/js/forTesting/validateForm.js
+++ assets/js/forTesting/validateForm.js
@@ -5,10 +5,34 @@ function validateForm(form) {
     var test_radioButton = form.elements['demo-priority'].checked; 
     var test_checkBox = form.elements['demo-human'].checked
 
-    if (test_name == "" || test_email == "" || test_dropDown == "" || !(test_radioButton) || !(test_checkBox)) {
-      alert("Fill out required fields!");
+    if (test_name === "") {
+      alert("Fill out name!");
+      return false;
+    } else if (test_email === "") {
+      alert("Fill out email!");
+      return false;
+    } else if (test_dropDown === "") {
+      alert("Select dropDown!");
+      return false;
+    } else if (!test_radioButton){
+      alert("Select radio button");
+      return false;
+    } else if (!test_checkBox){
+      alert("Are you human???");
       return false;
     } else {
-        return true;
+      return true;
     }
   }
+
+function validateForm2(form) {
+  var test_name = form.elements['name'].value;
+  var test_email = form.elements['email'].value;
+
+  if (test_name == "" || test_email == "") {
+    alert("Fill out required fields!");
+    return false;
+  } else {
+      return true;
+  }
+}
