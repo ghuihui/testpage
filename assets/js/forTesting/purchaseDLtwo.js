1f6dac8064f1ad77e624779ec63c12d8c5400c04
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-30 21:20:54 +0800

    Update purchaseDLtwo.js

--- assets/js/forTesting/purchaseDLtwo.js
+++ assets/js/forTesting/purchaseDLtwo.js
@@ -1,24 +1,10 @@
 dataLayer.push(
     {
         "0": "event",
-        "1": "page_view",
-        "2": {
-            "send_to": [
-                "G-ZZZ",
-                "MC-ZZZ"
-            ],
-            "page_path": "/checkout/thank_you",
-            "page_title": "Checkout - Receipt",
-            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
-            "ignore_referrer": "true"
-        }
-    },
-    {
-        "0": "event",
         "1": "purchase",
         "2": {
             "currency": "JPY",
-            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/zzzz/thank_you",
             "page_path": "/checkout/thank_you",
             "language": "ja",
             "page_title": "Checkout - Receipt",
@@ -27,7 +13,7 @@ dataLayer.push(
             "event_category": "EnhancedEcommerce",
             "allow_display_features": true,
             "allow_ad_personalization_signals": true,
-            "cookie_domain": "lucille.co.jp",
+            "cookie_domain": "test.co.jp",
             "cookie_expires": 63072000,
             "cookie_flags": "",
             "cookie_path": "/",
@@ -81,7 +67,7 @@ dataLayer.push(
         "1": "Completed Order",
         "2": {
             "currency": "JPY",
-            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/zzzz/thank_you",
             "page_path": "/checkout/thank_you",
             "language": "ja",
             "page_title": "Checkout - Receipt",
@@ -90,7 +76,7 @@ dataLayer.push(
             "event_category": "EnhancedEcommerce",
             "allow_display_features": true,
             "allow_ad_personalization_signals": true,
-            "cookie_domain": "lucille.co.jp",
+            "cookie_domain": "test.co.jp",
             "cookie_expires": 63072000,
             "cookie_flags": "",
             "cookie_path": "/",
@@ -105,7 +91,7 @@ dataLayer.push(
         "0": "event",
         "1": "page_view",
         "2": {
-            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
+            "page_location": "https://shop.lucille.co.jp/checkouts/cn/zzzz/thank_you",
             "page_path": "/checkout/thank_you",
             "language": "ja",
             "page_title": "Checkout - Receipt",
@@ -113,7 +99,7 @@ dataLayer.push(
             "client_id": "1679088078.1677654582",
             "allow_display_features": true,
             "allow_ad_personalization_signals": true,
-            "cookie_domain": "lucille.co.jp",
+            "cookie_domain": "test.co.jp",
             "cookie_expires": 63072000,
             "cookie_flags": "",
             "cookie_path": "/",
@@ -124,31 +110,6 @@ dataLayer.push(
             ]
         }
     },
-    {
-        "0": "event",
-        "1": "Viewed Checkout - Receipt Page",
-        "2": {
-            "page_location": "https://shop.lucille.co.jp/checkouts/cn/d953521058b3ef1fa9097027184388c7/thank_you",
-            "page_path": "/checkout/thank_you",
-            "language": "ja",
-            "page_title": "Checkout - Receipt",
-            "screen_resolution": "3200x1800",
-            "client_id": "1679088078.1677654582",
-            "event_category": "All",
-            "value": "0",
-            "allow_display_features": true,
-            "allow_ad_personalization_signals": true,
-            "cookie_domain": "lucille.co.jp",
-            "cookie_expires": 63072000,
-            "cookie_flags": "",
-            "cookie_path": "/",
-            "cookie_update": true,
-            "is_legacy_converted": true,
-            "send_to": [
-                "G-ZZZ"
-            ]
-        }
-    },   
     {
         "0": "event",
         "1": "purchase",
