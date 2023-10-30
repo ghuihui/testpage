d6bab4639f3725903581ca527cbc1fdc76dbd125
ghuihui <105323433+ghuihui@users.noreply.github.com>
2023-10-30 16:44:09 +0800

    Create view_item.js

---
+++ assets/js/forTesting/view_item.js
@@ -0,0 +1,63 @@
+dataLayer.push(
+  {
+          "ecommerce": {
+            "currencyCode": "USD",
+            "detail": {
+                "actionField": {
+                    "list": "Apparel Gallery"
+                },
+                "products": [
+                    {
+                        "name": "itemA",
+                        "id": "1111",
+                        "price": "2000",
+                        "variant": "Black Gold"
+                    }
+                ]
+            }
+        }
+  },
+  {
+    "PageType": "Productpage",
+    "ProductID": "1111"
+  },
+  {
+    "0": "event",
+    "1": "view_item",
+    "2": {
+        "send_to": [
+            "G-999",
+            "AW-999/XXX",
+            "MC-YYY"
+        ],
+        "ecomm_prodid": [
+            "1111"
+        ],
+        "ecomm_totalvalue": null,
+        "ecomm_pagetype": "product",
+        "items": [
+            {
+                "id": "1111",
+                "name": "itemA",
+                "brand": "brandA",
+                "category": "",
+                "price": "2000",
+                "variant": null
+            }
+        ]
+    }
+},
+  {
+    "event":"view_item",
+    "items": [
+            {
+                "id": "1111",
+                "name": "itemA",
+                "brand": "brandA",
+                "category": "",
+                "price": "2000",
+                "variant": null
+            }
+     ]
+  } 
+)
